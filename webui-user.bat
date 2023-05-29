9883644a934e008db584158e0842f4c1e29774a3
Thomas <thomaseding@gmail.com>
2023-05-29 13:49:23 -0700

    better cli args

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,15 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --no-download-sd-model --port=7860 --xformers
+set COMMANDLINE_ARGS= ^
+  --no-download-sd-model^
+  --data-dir=E:/models/a1111-user-data^
+  --ckpt-dir=E:/models/Stable-diffusion^
+  --hypernetwork-dir=E:/models/hypernetworks^
+  --lora-dir=E:/models/Lora^
+  --embeddings-dir=E:/models/embeddings^
+  --textual-inversion-templates-dir=E:/models/ti-templates^
+  --xformers^
+  --port=7860
 
 call webui.bat
