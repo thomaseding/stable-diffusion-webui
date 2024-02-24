54f42aa0fefc33766000fa1a6448e30a4d8ccb81
Thomas <thomaseding@gmail.com>
2024-02-23 16:59:55 -0800

    Update webui-user.bat with new directories

--- webui-user.bat
+++ webui-user.bat
@@ -5,12 +5,32 @@ set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS= ^
   --no-download-sd-model^
+  ^
   --data-dir=E:/models/a1111-user-data^
+  ^
   --ckpt-dir=E:/models/Stable-diffusion^
+  --ckpt-dir=E:/civitai/v1.5/checkpoint^
+  --ckpt-dir=E:/civitai/sdxl/checkpoint^
+  ^
+  --vae-dir=E:/civitai/v1.5/vae^
+  ^
   --hypernetwork-dir=E:/models/hypernetworks^
-  --lora-dir=E:/models/Lora^
-  --embeddings-dir=E:/models/embeddings^
+  --hypernetwork-dir=E:/civitai/v1.5/hypernetwork^
+  ^
+  --lora-dir=E:/models/lora^
+  --lora-dir=E:/civitai/v1.5/lora^
+  --lora-dir=E:/civitai/sdxl/lora^
+  ^
+  --lyco-dir=E:/models/lycoris^
+  --lyco-dir=E:/civitai/v1.5/lycoris^
+  --lyco-dir=E:/civitai/sdxl/lycoris^
+  ^
+  --embeddings-dir=E:/models/textual-inversion^
+  --embeddings-dir=E:/civitai/v1.5/textual-inversion^
+  --embeddings-dir=E:/civitai/sdxl/textual-inversion^
+  ^
   --textual-inversion-templates-dir=E:/models/ti-templates^
+  ^
   --xformers^
   --port=7860^
   ^
